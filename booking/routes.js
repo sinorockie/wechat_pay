3c3c0ae85c453ebb22501390ae41dcb3ed0ede8d
Lei Shi <luke.shi@proverseaedu.com>
2017-04-08 16:14:53 +0800

    install angularjs

--- booking/routes.js
+++ booking/routes.js
@@ -1,7 +1,9 @@
 var crypto = require('crypto');
 var express = require('express');
 module.exports = function(app){
-	app.use('/weui', express.static(__dirname + '/../node_modules/weui/dist/style'));
+	app.use('/weui',
+		express.static(__dirname + '/../node_modules/weui/dist/style')).use('/angular',
+		express.static(__dirname + '/../node_modules/angular'));
 	app.get('/', function(res, res){
 		res.render('index');
 	});
