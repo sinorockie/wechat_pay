f3d99b9d1083aee56e0dfdbc3230eac545dc9957
Lei Shi <luke.shi@proverseaedu.com>
2017-04-10 23:42:28 +0800

    ui

--- booking/routes.js
+++ booking/routes.js
@@ -3,6 +3,7 @@ var express = require('express');
 
 module.exports = function(app){
 	app.use('/weui', express.static(__dirname + '/../node_modules/weui/dist/style')).
+		use('/jquery', express.static(__dirname + '/../node_modules/jquery/dist')).
 		use('/angular', express.static(__dirname + '/../node_modules/angular')).
 		use('/static', express.static(__dirname + '/static'));
 
